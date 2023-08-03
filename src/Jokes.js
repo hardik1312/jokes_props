9c6fe6d2a2b1a2ff9030035a5cc3e634ac8d7a4f
Hardik Khatri <hardikkhatri1312@gmail.com>
2023-08-03 15:30:11 +0530

    Modified readme file

---
+++ src/Jokes.js
@@ -0,0 +1,11 @@
+function Jokes(props){
+    return(
+        <div>
+            {props.setup && <h3>Setup: {props.setup}</h3>}
+            <p>Punchline: {props.punchline}</p>
+            <hr />  
+        </div>
+    )
+}
+
+export default Jokes;
---
+++ src/jokesData.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        setup:'A man goes to the zoo and the only animal there is one dog.',
+        punchline:'It is a shitzhu.'
+    },
+    {
+        setup:'I told my wife she was drawing her eyebrows too high.',
+        punchline:'She looked surprised.'
+    },
+    {
+        setup:'Two cannibals are eating a clown.',
+        punchline:'One looks at the other and asks, "Does this taste funny to you?"'
+    },
+    {
+        setup:"They say I'm addicted to brake fluid.",
+        punchline:'I can stop anytime I want.'
+    },
+    {
+        setup:'What time does Sean Connery show up to Wimbledon?',
+        punchline:'Tennish'
+    }
+]
